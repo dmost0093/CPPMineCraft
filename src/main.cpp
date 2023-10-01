cc2602c4b6cacb034720de1747a97b4c934aca57
Sangil Han <sangilhan93@hotmail.com>
2023-10-01 15:44:59 -0700

    Add GLEW lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,57 @@
 #include <iostream>
 
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
-    GLFWwindow* window;
+	std::cout << "Hello World" << std::endl;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	GLFWwindow* window;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "640 x 480", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	/* glfwMakeContext() must be call after glewInit()*/
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "ERROR" << std::endl;
+	}
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+	std::cout << glGetString(GL_VERSION) << std::endl; // print the GL_VERSION of my platform
 
-    glfwTerminate();
-    return 0;
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Lagacy code to draw a triangle */
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5, -0.5f);
+		glEnd();
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
